38d7f09c1bb9df03cae431da9c2d7841d5d84762
Fouad Elamoury <foad.osama@hotmail.com>
2024-11-11 22:10:40 +0200

    Drawing Arc

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,45 @@
 #include <SFML/Graphics.hpp>
+#include <cmath>
+
+#define PI 3.14159265f
+
+void updateArc(sf::ConvexShape& shape, const sf::Vector2f& center, float radius, float startAngle, float endAngle)
+{
+    int pointCount = shape.getPointCount() - 1;  
+    float angleStep = (endAngle - startAngle) / (pointCount - 1);
+
+    for (int i = 0; i < pointCount; ++i)
+    {
+        float angle = startAngle + i * angleStep;
+        float radian = angle * (PI / 180.f);  
+
+        float x = center.x + radius * std::cos(radian);
+        float y = center.y + radius * std::sin(radian);
+
+        shape.setPoint(i, sf::Vector2f(x, y));
+    }
+
+    shape.setPoint(pointCount, center);
+}
 
 int main()
 {
-    auto window = sf::RenderWindow({1920u, 1080u}, "CMake SFML Project");
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "TheArc");
     window.setFramerateLimit(144);
+
+    sf::ConvexShape shape(51);  
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Vector2f center(400, 400); 
+
+    shape.setOrigin(center);
+    shape.setPosition(window.getSize().x / 2.0f, window.getSize().y / 2.0f);
+
+    updateArc(shape, center, 200, -90, 0);  
 
     while (window.isOpen())
     {
-        for (auto event = sf::Event(); window.pollEvent(event);)
+        for (sf::Event event; window.pollEvent(event);)
         {
             if (event.type == sf::Event::Closed)
             {
@@ -16,6 +48,9 @@ int main()
         }
 
         window.clear();
+        window.draw(shape);
         window.display();
     }
+
+    return 0;
 }
