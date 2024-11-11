6f5eb01b6b8711d6dcdd583f3c2138430818f54f
Fouad Elamoury <foad.osama@hotmail.com>
2024-11-11 19:17:06 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
+    sf::CircleShape circle;
+    circle.setRadius(100);
+    circle.setOrigin({circle.getRadius(), circle.getRadius()});
+    circle.setPosition({1920u / 2, 1080u / 2});
+    circle.setFillColor(sf::Color::Red);
+
     auto window = sf::RenderWindow({1920u, 1080u}, "CMake SFML Project");
     window.setFramerateLimit(144);
 
